5bb3723de898ed8ee07a92af256ab54bcc112535
Laura <lauradelpinodiaz94@gmail.com>
2017-01-20 19:50:07 +0100

    Reference background

---
+++ Kinesis/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void changeScene(string scene){
+		if (scene.Equals ("Quit"))
+			Application.Quit ();
+		else {
+			SceneManager.LoadScene (scene);
+		}
+	}
+
+
+}
