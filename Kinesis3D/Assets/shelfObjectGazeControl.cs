81cd098a93c3df1fde056ecec908288e75c3f11c
Laura <lauradelpinodiaz94@gmail.com>
2017-01-21 11:24:29 +0100

    Kiness un Unity 5.4

---
+++ Kinesis3D/Assets/shelfObjectGazeControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tobii.EyeTracking;
+
+
+public class shelfObjectGazeControl : MonoBehaviour {
+
+    private GazeAware _gazeAware;
+
+    // Use this for initialization
+    void Start () {
+        _gazeAware = GetComponent<GazeAware>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (_gazeAware.HasGazeFocus)
+        {
+            Debug.Log("OK");
+        }
+        else
+            Debug.Log("ERROR");
+    }
+}
