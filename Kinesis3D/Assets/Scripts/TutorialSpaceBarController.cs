fb4caa46aba4bbed239a67a3eba1820363eca863
Laura <lauradelpinodiaz94@gmail.com>
2017-02-05 23:05:35 +0100

    Skip Tutorial

---
+++ Kinesis3D/Assets/Scripts/TutorialSpaceBarController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TutorialSpaceBarController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+			SceneManager.LoadScene("Game");
+	}
+}
