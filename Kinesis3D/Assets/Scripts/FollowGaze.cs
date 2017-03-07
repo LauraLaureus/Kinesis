aafb5fd154482cd3f40e7b97e0dd928e44205101
Laura <lauradelpinodiaz94@gmail.com>
2017-03-07 21:44:44 +0100

    Update Script

--- Kinesis3D/Assets/Scripts/FollowGaze.cs
+++ Kinesis3D/Assets/Scripts/FollowGaze.cs
@@ -6,6 +6,6 @@ public class FollowGaze : MonoBehaviour {
 	public void moveTorwards(Vector3 v){
 
 		//TODO ignore Z axis
-		this.transform.position = Vector3.MoveTowards (this.transform.position, v, 1 / Mathf.Abs (Vector3.Magnitude (transform.position, v)));
+		this.transform.position = new Vector3(v.x,v.y,-6f);
 	}
 }
